2b66a53df6d8a214eb3597f6e0d874cc0722f649
Nickolas A. Morrison <nickmorrison09@gmail.com>
2017-02-13 19:32:23 -0500

    New Phaseone Site

---
+++ js/calendar.js
@@ -0,0 +1,8 @@
+var app = angular.module("PhaseOne");
+app.controller("CalendarController",function($scope){
+	/*$scope.month = "";
+	setTimeout(function(){				
+		var iframe = $("iframe").contents();		
+		$scope.month = iframe.find("#currentDate1").html().split(" ")[0];
+		},500);*/
+});
